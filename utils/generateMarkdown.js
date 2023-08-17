d1a8523a2c389d05bb8428d29b6e4d203166a99c
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-08-17 04:46:48 -0500

    finished out func generatemarkdown, need to work on licensing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,11 +13,39 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   let tableOfContents = '';
-  
+
+  if (data.toc && data.toc.length > 0) {
+    tableOfContents = '## Table of Contents\n';
+
+    data.toc.forEach((section) => {
+      tableOfContents += `- [${section}](#${section.toLowerCase()})\n`;
+    });
+  }
 
   return `# ${data.title}
 
-`;
+${tableOfContents}
+
+## Description
+${data.desc}
+
+## Installation
+${data.install}
+
+## License
+${renderLicenseBadge(data.license)}  ${renderLicenseLink(data.license)}
+
+## Usage
+${data.use}
+
+## Tests
+${data.testing}
+
+## Contributing
+${data.contribution}
+
+## Questions
+For questions, you can reach me at [GitHub Profile](https://github.com/${data.username}) or via email at ${data.email}.`;
 }
 
 module.exports = generateMarkdown;
